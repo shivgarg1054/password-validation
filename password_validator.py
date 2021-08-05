2190b2193fc935d28e1bfbb72bb62d305bf7d4ac
Shivam Garg <shivgarg@adobe.com>
2021-08-05 15:35:59 +0530

    added password validator requirements

--- password_validator.py
+++ password_validator.py
@@ -3,6 +3,7 @@ from collections import Counter
 def password_check(passwd):
     SpecialSym = ['!', '@', '#', '$', '&', '*']
     value = True
+    upper, lower, number, special, count, total = 0, 0, 0, 0, 0, 0
 
     res = Counter(passwd)
     x = max(res, key=res.get)
@@ -17,15 +18,20 @@ def password_check(passwd):
     if not any(char.isdigit() for char in passwd):
         print('Password should have at least one numeral')
         return False
-    count = 0
-    for char in passwd:
-        if (char.isdigit()):
-            count = count + 1
-        if count >= len(passwd)/2:
-            print(count)
-            return False
+    
+    for char in range(len(passwd)):
+        if passwd[char].isupper():
+            upper += 1
+        elif passwd[char].islower():
+            lower += 1
+        elif passwd[char].isdigit():
+            number += 1
         else:
-            value = True
+            special += 1 
+
+    if (number >= len(passwd) / 2):
+        print("To check 50 % of password should not be a number")
+        return False
 
     if not any(char.isupper() for char in passwd):
         print('Password should have at least one uppercase letter')
@@ -38,23 +44,28 @@ def password_check(passwd):
     if not any(char in SpecialSym for char in passwd):
         print('Password should have at least one of the symbols $@#')
         return False
-    count = 0
+
     for char in passwd:
         if char in SpecialSym:
             count = count + 1
-            print(count)
-
-        if count > 4:
-            print("sad")
-            return False
         else:
-            value = True
+            pass
+    
+    if (count > 4):
+        return False
+
+    total = upper + lower + number + count 
+
+    if (len(passwd) - total != 0):
+        print("invalid characters in password")
+        return False
 
     if value:
         return value
+
 # Main method
 def main():
-    passwd = 'zaq%qx$wC%EVF&12345#'
+    passwd = 'zaqqx$wC$E%VF&12345#'
 
     if (password_check(passwd)):
         print("Password is valid")
