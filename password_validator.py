f6bc60dbcae188f6f97c5b7eb1e4be169816ab35
Shivam Garg <shivgarg@adobe.com>
2021-08-05 16:37:38 +0530

    added password validator requirements

--- password_validator.py
+++ password_validator.py
@@ -1,15 +1,20 @@
 from collections import Counter
+import unittest
+
+
 # Function to validate the password
-def password_check(passwd):
+def validate(passwd):
     SpecialSym = ['!', '@', '#', '$', '&', '*']
     value = True
     upper, lower, number, special, count, total = 0, 0, 0, 0, 0, 0
 
-    res = Counter(passwd)
-    x = max(res, key=res.get)
-    print(x)
-    print(str(res[x]))
-
+    if (passwd != ""):
+        res = Counter(passwd)
+        x = max(res, key=res.get)
+        y = str(res[x])
+        if y > '4':
+            print('duplicate repeat characters more than 4')
+            return False
 
     if len(passwd) < 17:
         print('length should be at least 18')
@@ -18,7 +23,7 @@ def password_check(passwd):
     if not any(char.isdigit() for char in passwd):
         print('Password should have at least one numeral')
         return False
-    
+
     for char in range(len(passwd)):
         if passwd[char].isupper():
             upper += 1
@@ -27,7 +32,7 @@ def password_check(passwd):
         elif passwd[char].isdigit():
             number += 1
         else:
-            special += 1 
+            special += 1
 
     if (number >= len(passwd) / 2):
         print("To check 50 % of password should not be a number")
@@ -50,11 +55,11 @@ def password_check(passwd):
             count = count + 1
         else:
             pass
-    
+
     if (count > 4):
         return False
 
-    total = upper + lower + number + count 
+    total = upper + lower + number + count
 
     if (len(passwd) - total != 0):
         print("invalid characters in password")
@@ -63,21 +68,41 @@ def password_check(passwd):
     if value:
         return value
 
+
 # Main method
-def main():
-    passwd = 'qwertyPOIUYT12345@#$'
+class ValidateTests(unittest.TestCase):
 
-    passwd = ['qwertyPOIUYT12345@#$','qweqhasjd21376@#$&','asd12@&','zaqxsw12341234@#$$']
+    def test_empty(self):
+        self.assertFalse(validate(''))
 
-    for i in range(0, len(passwd)):
-        if (password_check(passwd[i])):
-            print(passwd[i])
-            print("Password is valid")
-        else:
-            print(passwd[i])
-            print("Invalid Password !!")
+    def test_too_short(self):
+        self.assertFalse(validate('aAbB1!?'))
+
+    def test_too_long(self):
+        self.assertTrue(validate('zaqxswcdevfrBGTNHYMJUKILOp1234567890@#$&'))
+        print("valid")
+
+    def test_no_number(self):
+        self.assertFalse(validate("zaqxscdevfrbgtnhyqw$$&"))
+
+    def test_no_upper(self):
+        self.assertFalse(validate("aabbzzqwetuwqte"))
+
+    def test_no_lower(self):
+        self.assertFalse(validate("%&AABBCCQWERTY1234$"))
+
+    def test_no_special(self):
+        self.assertFalse(validate("aAbmMC19zaqxswASD12"))
+
+    def test_duplicate_repeat(self):
+        self.assertFalse(validate('aaaaaaaZAQXSWCDE1234#$'))
+        print("Failed here")
+
+    def test_valid(self):
+        self.assertTrue(validate("qwertyPOIUYT12345@#$"))
+        print("valid")
 
 
 # Driver Code
 if __name__ == '__main__':
-    main()
+    unittest.main()
